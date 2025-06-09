db9d6a3c20ffb111cc821f4af676b08c5b700f7e
Dobix <dobmax@proton.me>
2025-06-09 12:13:46 +0200

    screen size adjustments for player hand

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,10 +1,18 @@
 class_name Card
 extends Panel
 
-const SIZE := Vector2(100, 140)
-
 @export var image: Texture2D
 @onready var texture_rect: TextureRect = $TextureRect
 
 func _ready() -> void:
 	texture_rect.texture = image
+
+func apply_card_size(size: Vector2) -> void:
+	custom_minimum_size = size
+	texture_rect.custom_minimum_size = size
+
+func get_scaled_size(hand_size: Vector2) -> Vector2:
+	var scale_factor := 0.9  # 90 % der Höhe
+	var card_height := hand_size.y * scale_factor
+	var aspect_ratio := 120.0 / 180.0  # original width / height
+	return Vector2(card_height * aspect_ratio, card_height)
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -11,6 +11,22 @@ const CARD = preload("res://scenes/nostra/card.tscn")
 @export var y_min := 0
 @export var y_max := -15
 
+func _ready() -> void:
+	_update_hand_size()
+
+func _notification(what):
+	if what == NOTIFICATION_RESIZED:
+		_update_hand_size()
+		_update_cards()
+
+func _update_hand_size() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	size = Vector2(screen_size.x * 0.65, screen_size.y * 0.25)
+	position = Vector2(
+		(screen_size.x - size.x) / 2,
+		screen_size.y - size.y
+	)
+
 func draw_card() -> void:
 	var new_card = CARD.instantiate()
 	#new_card.image = "res://assets/nostra/cards/01-blue.jpg"
@@ -28,26 +44,34 @@ func discard_card() -> void:
 
 func _update_cards() -> void:
 	var cards := get_child_count()
-	var all_cards_size := Card.SIZE.x * cards + x_sep * (cards - 1)
+	if cards == 0:
+		return
+	
+	# Neue dynamische Kartengröße berechnen
+	var card_size := Card.new().get_scaled_size(size)
+	
+	# Diese Zeile ersetzt Card.SIZE überall im Code:
+	var all_cards_size := card_size.x * cards + x_sep * (cards - 1)
 	var final_x_sep := x_sep
 	
 	if all_cards_size > size.x:
-		final_x_sep = (size.x - Card.SIZE.x * cards) / (cards - 1)
+		final_x_sep = (size.x - card_size.x * cards) / (cards - 1)
 		all_cards_size = size.x
 		
 	var offset := (size.x - all_cards_size) / 2
 	
 	for i in cards:
 		var card := get_child(i)
-		var y_multiplier := hand_curve.sample(1.0 / (cards-1) * i)
-		var rot_multiplier := rotation_curve.sample(1.0 / (cards-1) * i)
 		
-		if cards == 1:
-			y_multiplier = 0.6
-			rot_multiplier = 0.0
-			
-		var final_x: float = offset + Card.SIZE.x * i + final_x_sep * i
+		var y_multiplier := hand_curve.sample(1.0 / (cards-1) * i) if cards > 1 else 0.6
+		var rot_multiplier := rotation_curve.sample(1.0 / (cards-1) * i) if cards > 1 else 0.0
+		
+		# Positionieren
+		var final_x: float = offset + card_size.x * i + final_x_sep * i
 		var final_y: float = y_min + y_max * y_multiplier
 		
 		card.position = Vector2(final_x, final_y)
 		card.rotation_degrees = max_rotation_degrees * rot_multiplier
+		
+		# Dynamische Größe setzen
+		card.set_size(card_size)
