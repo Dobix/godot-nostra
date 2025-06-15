2328ac47ddba745ae6a42ace548b4c50879df36f
Dobix <dobmax@proton.me>
2025-06-15 12:38:15 +0200

    added game over handling

--- scripts/nostra/nostra_game.gd
+++ scripts/nostra/nostra_game.gd
@@ -15,7 +15,6 @@ extends CanvasLayer
 @onready var next_turn_button: Button = $Button_Next_Turn
 @onready var show_end_result_button: Button = $Button_show_end_result
 
-
 enum Turn { PLAYER, NPC }
 var current_turn: Turn
 var round_active := false
@@ -39,10 +38,8 @@ var attacker_decision: String
 
 var player_discard_pile: Array[CardData] = []
 var npc_discard_pile: Array[CardData] = []
-var discard
 
 var round_just_ended := false
-
 var needed_scores
 
 func start_nostra(npc_name: String, difficulty: int, npc_portrait: Texture2D, win_multiplier: float):
@@ -54,20 +51,18 @@ func start_nostra(npc_name: String, difficulty: int, npc_portrait: Texture2D, wi
 	$Enemy_Info/Enemy_Portrait.texture = npc_portrait
 	$Player_Info/VBoxContainer/Player_Name.text = "Du"
 	$Player_Info/Player_Portrait.texture = npc_portrait
-	
+
 	var full_deck = deck_manager.get_deck()
 	player_deck = full_deck.duplicate()
 	npc_deck = full_deck.duplicate()
 	player_deck.shuffle()
 	npc_deck.shuffle()
-	
-	var deck_age_sum = score_handler.sum_card_age(player_deck)
-	
-	needed_scores = score_handler.get_needed_scores(win_multiplier, deck_age_sum)
-	$Enemy_Info/VBoxContainer/Enemy_Score.text = "0/"+str(needed_scores.npc_needed_score)
-	$Player_Info/VBoxContainer/Player_Score.text = "0/"+str(needed_scores.player_needed_score)
 
-	
+	var deck_age_sum = score_handler.sum_card_age(player_deck)
+	needed_scores = score_handler.get_needed_scores(win_multiplier, deck_age_sum)
+
+	update_score_labels(0, 0)
+
 	player_hand = deck_manager.draw_cards_from_deck(player_deck, 3)
 	npc_hand = deck_manager.draw_cards_from_deck(npc_deck, 3)
 
@@ -75,6 +70,10 @@ func start_nostra(npc_name: String, difficulty: int, npc_portrait: Texture2D, wi
 		hand.draw_card(card_data)
 	for card_data in npc_hand:
 		enemy_hand.draw_card(card_data)
+
+func update_score_labels(player_score: int, npc_score: int):
+	$Player_Info/VBoxContainer/Player_Score.text = str(player_score) + "/" + str(needed_scores.player_needed_score)
+	$Enemy_Info/VBoxContainer/Enemy_Score.text = str(npc_score) + "/" + str(needed_scores.npc_needed_score)
 
 func show_card_popup(card_data: CardData):
 	selected_popup_card = card_data
@@ -159,13 +158,12 @@ func evaluate_round(player: String, card_data: CardData, decision: String):
 		attacker_card_data = card_data
 		attacker_decision = decision
 		if current_defender == "NPC":
-			remove_card_from_player_hand(card_data.id)  # HIER
+			remove_card_from_player_hand(card_data.id)
 			respond_npc()
 	else:
 		defender_card_data = card_data
-		remove_card_from_player_hand(card_data.id)  # HIER
+		remove_card_from_player_hand(card_data.id)
 		resolve_round(attacker_card_data, defender_card_data, attacker_decision, decision)
-
 
 func resolve_round(card1: CardData, card2: CardData, decision1: String, decision2: String):
 	npc_decision_label.hide()
@@ -175,22 +173,31 @@ func resolve_round(card1: CardData, card2: CardData, decision1: String, decision
 	var attacker_wins = result.attacker_wins
 	var defender_wins = result.defender_wins
 
-	var result_text = ""
-	result_text += current_attacker + " sagt: Meine Karte (" + str(card1.value) + ") ist " + decision1 + "\n"
-	result_text += current_defender + " sagt: Meine Karte (" + str(card2.value) + ") ist " + decision2 + "\n\n"
+	var result_text = """
+		%s sagt: Meine Karte (%s) ist %s
+		%s sagt: Meine Karte (%s) ist %s
+
+		""" % [current_attacker, card1.value, decision1, current_defender, card2.value, decision2]
 
 	if attacker_wins and defender_wins:
 		result_text += "Beide hatten recht!"
-		_add_to_discard(current_attacker, card1)
-		_add_to_discard(current_defender, card2)
+		add_cards_to_discard([
+			{"who": current_attacker, "card": card1},
+			{"who": current_defender, "card": card2}
+		])
 	elif attacker_wins:
 		result_text += current_attacker + " gewinnt die Runde!"
-		_add_to_discard(current_attacker, card1)
-		_add_to_discard(current_attacker, card2)
+		add_cards_to_discard([
+			{"who": current_attacker, "card": card1},
+			{"who": current_attacker, "card": card2}
+		])
+
 	elif defender_wins:
 		result_text += current_defender + " gewinnt die Runde!"
-		_add_to_discard(current_defender, card1)
-		_add_to_discard(current_defender, card2)
+		add_cards_to_discard([
+			{"who": current_defender, "card": card1},
+			{"who": current_defender, "card": card2}
+		])
 	else:
 		result_text += "Niemand hat recht!"
 		if current_attacker == "Spieler":
@@ -206,21 +213,24 @@ func resolve_round(card1: CardData, card2: CardData, decision1: String, decision
 
 	var player_empty = player_hand.is_empty() and player_deck.is_empty()
 	var npc_empty = npc_hand.is_empty() and npc_deck.is_empty()
-	
+
 	if player_empty and npc_empty:
 		next_turn_button.hide()
 		show_end_result_button.show()
 	else:
 		next_turn_button.show()
 
-func _add_to_discard(who: String, card: CardData):
-	if who == "Spieler":
-		player_discard_pile.append(card)
-	elif who == "NPC":
-		npc_discard_pile.append(card)
+func add_cards_to_discard(pairs: Array[Dictionary]):
+	for entry in pairs:
+		var who = entry["who"]
+		var card = entry["card"]
+		if who == "Spieler":
+			player_discard_pile.append(card)
+		elif who == "NPC":
+			npc_discard_pile.append(card)
+
 	var scores = score_handler.get_current_scores(player_discard_pile, npc_discard_pile)
-	$Player_Info/VBoxContainer/Player_Score.text = str(scores.player_score)+"/"+str(needed_scores.player_needed_score)
-	$Enemy_Info/VBoxContainer/Enemy_Score.text = str(scores.npc_score)+"/"+str(needed_scores.npc_needed_score)
+	update_score_labels(scores.player_score, scores.npc_score)
 
 
 func _insert_card_back(deck: Array[CardData], card: CardData):
@@ -235,13 +245,8 @@ func _on_button_next_turn_pressed():
 	next_turn()
 
 func _on_button_show_end_result_pressed():
-	var player_age_total := 0
-	for card in player_discard_pile:
-		player_age_total += card.age
-
-	var npc_age_total := 0
-	for card in npc_discard_pile:
-		npc_age_total += card.age
+	var player_age_total = score_handler.sum_card_age(player_discard_pile)
+	var npc_age_total = score_handler.sum_card_age(npc_discard_pile)
 
 	var result := "Spieler hat " + str(player_age_total) + " Punkte erreicht.\n"
 	result += "NPC hat " + str(npc_age_total) + " Punkte erreicht.\n\n"
@@ -253,23 +258,24 @@ func _on_button_show_end_result_pressed():
 	else:
 		result += "Unentschieden!"
 
-	show_game_over(result)
+	print("show end result")
 
+#func show_game_over(text: String):
+	#turn_label.text = ""
+	#npc_decision_label.hide()
+	#round_result_label.text = text
+	#round_result_label.show()
+	#hand.allowed_to_interact = false
+	#next_turn_button.hide()
+	#show_end_result_button.hide()
+	
 func next_turn():
 	round_active = false
-	if current_turn == Turn.PLAYER:
-		_start_npc_turn()
-	else:
-		_start_player_turn()
-
-func show_game_over(text: String):
-	turn_label.text = ""
-	npc_decision_label.hide()
-	round_result_label.text = text
-	round_result_label.show()
-	hand.allowed_to_interact = false
-	next_turn_button.hide()
-	show_end_result_button.hide()
+	match current_turn:
+		Turn.PLAYER:
+			_start_npc_turn()
+		Turn.NPC:
+			_start_player_turn()
 
 func remove_card_from_player_hand(card_id: int):
 	for card in hand.get_children():
@@ -287,8 +293,7 @@ func draw_cards_if_possible():
 		player_hand.append(card_data)
 		hand.draw_card(card_data)
 		$Deck_Pile/Deck_Pile_Sum.text = str(player_deck.size())
-		print(player_deck.size())
-		
+
 	if not npc_deck.is_empty():
 		var card_data: CardData = npc_deck.pop_front()
 		npc_hand.append(card_data)
--- scripts/nostra/nostra_score_handler.gd
+++ scripts/nostra/nostra_score_handler.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var player_needed_score: int
+var npc_needed_score: int
+enum winner { PLAYER, NPC }
+
 func sum_card_age(deck: Array[CardData]) -> int:
 	var total := 0
 	for card in deck:
@@ -9,16 +13,22 @@ func sum_card_age(deck: Array[CardData]) -> int:
 func get_current_scores(player_discard: Array[CardData], npc_discard: Array[CardData]) -> Dictionary:
 	var player_current_score := sum_card_age(player_discard)
 	var npc_current_score := sum_card_age(npc_discard)
-	
+	if player_current_score >= player_needed_score:
+		handle_gameover(winner.PLAYER)
+	elif npc_current_score >= npc_needed_score:
+		handle_gameover(winner.NPC)
 	return {
 		"player_score": player_current_score,
 		"npc_score": npc_current_score
 	}
 
+func handle_gameover(winner):
+	print("game over")
+
 
 func get_needed_scores(win_mulitplier: float, deck_age_sum: int) -> Dictionary:
-	var player_needed_score = int(deck_age_sum * win_mulitplier)
-	var npc_needed_score = int(deck_age_sum * win_mulitplier - deck_age_sum)
+	player_needed_score = (deck_age_sum * win_mulitplier)
+	npc_needed_score = (deck_age_sum * win_mulitplier - deck_age_sum)
 	return {
 		"player_needed_score": player_needed_score,
 		"npc_needed_score": npc_needed_score
