1506cc3e6e7668997655bca876b02718a1ff3859
Dobix <dobmax@proton.me>
2025-06-29 20:31:10 +0200

    added double click to playerhand

--- nostra/card/card.gd
+++ nostra/card/card.gd
@@ -19,6 +19,9 @@ var dragging_allowed := true
 
 var hand_ref: Hand = null  # oder einfach Node
 
+var hold_timer := 0.0
+var hold_threshold := 0.09  # Sekunden, z. B. 0.5–1.0
+
 signal card_selected(card: Card)
 
 func _ready() -> void:
@@ -38,18 +41,25 @@ func _physics_process(delta: float) -> void:
 func drag_logic(delta: float) -> void:
 	if not dragging_allowed:
 		return
+
 	var is_hovered_or_dragging := mouse_in or is_dragging
 	var is_dragged_by_self := GameManager.node_being_dragged == null or GameManager.node_being_dragged == self
+
 	if is_hovered_or_dragging and is_dragged_by_self:
 		if Input.is_action_pressed("left_click"):
 			if not is_dragging:
-				original_rotation = rotation
-				original_position = position
-				rotation = 0
-			global_position = lerp(global_position, get_global_mouse_position() - (size / 2.0), 22.0 * delta)
-			is_dragging = true
-			GameManager.node_being_dragged = self
+				hold_timer += delta
+				if hold_timer >= hold_threshold:
+					original_rotation = rotation
+					original_position = position
+					rotation = 0
+					is_dragging = true
+					GameManager.node_being_dragged = self
+			else:
+				global_position = lerp(global_position, get_global_mouse_position() - (size / 2.0), 22.0 * delta)
 		else:
+			# Loslassen: Reset
+			hold_timer = 0.0
 			is_dragging = false
 			if GameManager.node_being_dragged == self:
 				GameManager.node_being_dragged = null
@@ -78,6 +88,37 @@ func drag_logic(delta: float) -> void:
 					var tween := get_tree().create_tween()
 					tween.tween_property(self, "position", original_position, 0.2)
 
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.double_click:
+		handle_double_click()
+
+func handle_double_click():
+	if not dragging_allowed:
+		return
+
+	if card_display_ref == null:
+		print("❌ Card_Display Referenz fehlt!")
+		return
+
+	var target_slot = card_display_ref.get_node("Card_Display_grid/Player_Slot")
+	reparent(target_slot)
+	await get_tree().process_frame
+
+	var target_pos = target_slot.size / 2 - size / 2
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", target_pos, 0.2)
+
+	rotation = 0
+	hover_enabled = false
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	if is_instance_valid(hand_ref):
+		await get_tree().process_frame
+		hand_ref._update_cards()
+
+	emit_signal("card_selected", self)
 
 func _on_mouse_entered() -> void:
 	if hover_enabled:
